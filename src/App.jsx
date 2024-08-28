284d98f0b7b4558515dc7e3e64662055ca09c680
Harry Seigne <harryseigne@gmail.com>
2024-08-28 21:02:34 +1000

    Switch to mantine instead of bootstrap, select and display airports in an autocomplete component as a demo

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import GoogleAuth from "./components/auth/GoogleAuth.jsx";
 import {useEffect, useState} from "react";
 import {supabase} from "./supabaseClient.js";
 import Signout from "./components/auth/Signout.jsx";
-
+import '@mantine/core/styles.css';
+import {MantineProvider, Autocomplete} from '@mantine/core';
 
 function App() {
     const [session, setSession] = useState(null)
-    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [data, setData] = useState([])
 
     useEffect(() => {
         supabase.auth.getSession().then(({data: {session}}) => {
@@ -24,15 +26,42 @@ function App() {
     }, [])
 
     useEffect(() => {
-        supabase.auth.getUser().then(({data: {user}}) => setUser(user))
-    }, [])
+        let ignore = false
+
+        async function retrieveData() {
+            setLoading(true)
+            const {data, error} = await supabase
+                .from('airport')
+                .select('code')
+
+            if (!ignore) {
+                if (error) {
+                    console.warn(error)
+                } else if (data) {
+                    console.log(data)
+                    setData(data.map(value => value.code))
+                }
+            }
+            setLoading(false)
+        }
+
+        retrieveData()
+
+        return () => {
+            ignore = true
+        }
+    }, [session])
 
     return (
-        <div className="container">
-            <h1>Welcome to Point2Point</h1>
-            {null == session && <GoogleAuth/>}
-            {null != session && <Signout/>}
-        </div>
+        <MantineProvider>
+            <div className="container">
+                <h1>Welcome to Point2Point</h1>
+                {null == session && <GoogleAuth/>}
+                {null != session && <Signout/>}
+                {!loading && <Autocomplete label="Your favorite library"
+                                           placeholder="Pick value or enter anything" data={data}/>}
+            </div>
+        </MantineProvider>
     )
 }
 
